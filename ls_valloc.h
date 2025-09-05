ea8f734eddb00d93f2d5ad16a52f31edc06c5373
Logan Seeley <150400211+SeeleyLogan@users.noreply.github.com>
2025-09-05 10:17:15 -0400

    Changed valloc.h license

--- ls_valloc.h
+++ ls_valloc.h
@@ -217,10 +217,18 @@ inline ls_u64_t _ls_valloc_memtotal()
 
 
 /*
- * Copyright 2025, Logan Seeley
+ * Copyright (C) 2025  Logan Seeley
  *
- * Copying and distribution of this file, with or without modification,
- * are permitted in any medium without royalty, provided the copyright
- * notice and this notice are preserved. This file is offered as-is, 
- * without any warranty.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
