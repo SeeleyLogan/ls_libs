3cc06c2b4a9820f09f2a7ee567592e8966c3db30
Logan Seeley <150400211+SeeleyLogan@users.noreply.github.com>
2025-09-16 08:33:31 -0400

    Removed inline asm, _GNU_SOURCE must be defined globally.

--- ls_valloc.h
+++ ls_valloc.h
@@ -3,6 +3,8 @@
  *
  * Documentation
  *
+ *  IMPORTANT: When compiling with strict C standards, _GNU_SOURCE must be defined at compile time.
+ *
  *  Functions
  * 
  *      ls_vptr_t ls_valloc_vmalloc(ls_u64_t *size) - vmalloc
@@ -105,10 +107,6 @@ void        ls_valloc_pfree_range(ls_vptr_t ptr, ls_u64_t offset, ls_u64_t range
 
     #define ls_valloc_pcommit_range(...) _ls_valloc_pcommit_range_win32(__VA_ARGS__)
 #else
-    #define _LS_VALLOC_MADV_DONTNEED 4
-
-    static long _ls_valloc_madvise(void *addr, size_t length, int advice);
-
     #define ls_valloc_pcommit_range(...)  // still good practice to call this
 #endif
 
@@ -144,7 +142,7 @@ inline void ls_valloc_pfree(ls_vptr_t ptr)
     #ifdef _WIN32
         VirtualFree(ptr, _ls_valloc_memtotal(), MEM_DECOMMIT);
     #else
-        _ls_valloc_madvise(ptr, _ls_valloc_memtotal(), _LS_VALLOC_MADV_DONTNEED);
+        madvise(ptr, _ls_valloc_memtotal(), MADV_DONTNEED);
     #endif
 }
 
@@ -165,7 +163,7 @@ inline void ls_valloc_pfree_range(ls_vptr_t ptr, ls_u64_t offset, ls_u64_t range
     #ifdef _WIN32
         VirtualFree(_LS_CAST(_LS_CAST(ptr, ls_u64_t) + offset, ls_vptr_t), range, MEM_DECOMMIT);
     #else
-        _ls_valloc_madvise(_LS_CAST(_LS_CAST(ptr, ls_u64_t) + offset, ls_vptr_t), range, _LS_VALLOC_MADV_DONTNEED);
+        madvise(_LS_CAST(_LS_CAST(ptr, ls_u64_t) + offset, ls_vptr_t), range, MADV_DONTNEED);
     #endif
 }
 
@@ -187,79 +185,6 @@ inline void _ls_valloc_pcommit_range_win32(ls_vptr_t ptr, ls_u64_t offset, ls_u6
     
     VirtualAlloc(_LS_CAST(_LS_CAST(ptr, ls_u64_t) + offset, ls_vptr_t), range, MEM_COMMIT, PAGE_READWRITE);
 }
-#else
-
-static inline long _ls_valloc_madvise(void *addr, size_t length, int advice)
-{
-    long ret;
-
-#if defined(__x86_64__)
-
-    const long syscall_nr = 28;
-
-    __asm__ volatile (
-        "syscall"
-        : "=a" (ret)
-        : "a"(syscall_nr),
-          "D"(addr),
-          "S"(length),
-          "d"(advice)
-        : "rcx", "r11", "memory"
-    );
-
-#elif defined(__i386__)
-
-    const long syscall_nr = 219;
-
-    __asm__ volatile (
-        "int $0x80"
-        : "=a" (ret)
-        : "a"(syscall_nr),
-          "b"(addr),
-          "c"(length),
-          "d"(advice)
-        : "memory"
-    );
-
-#elif defined(__aarch64__)
-
-    register long x0 __asm__("x0") = (long)addr;
-    register long x1 __asm__("x1") = (long)length;
-    register long x2 __asm__("x2") = (long)advice;
-    register long x8 __asm__("x8") = 233;
-
-    __asm__ volatile (
-        "svc #0"
-        : "=r" (x0)
-        : "r" (x0), "r" (x1), "r" (x2), "r" (x8)
-        : "memory"
-    );
-
-    ret = x0;
-
-#elif defined(__arm__)
-
-    register long r0 __asm__("r0") = (long)addr;
-    register long r1 __asm__("r1") = (long)length;
-    register long r2 __asm__("r2") = (long)advice;
-    register long r7 __asm__("r7") = 192;
-
-    __asm__ volatile (
-        "swi 0"
-        : "=r"(r0)
-        : "r"(r0), "r"(r1), "r"(r2), "r"(r7)
-        : "memory"
-    );
-
-    ret = r0;
-
-#else
-    #error "ls_valloc.h does not support this architecture"
-#endif
-
-    return ret;
-}
-
 #endif
 
 
